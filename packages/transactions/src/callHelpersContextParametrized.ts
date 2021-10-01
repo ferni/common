75bba029312581dd27f329ec99ea52d646ebeec7
Adam Skrodzki <adamskrodzki@gmail.com>
2021-10-01 21:46:14 +0200

    add loging

--- packages/transactions/src/callHelpersContextParametrized.ts
+++ packages/transactions/src/callHelpersContextParametrized.ts
@@ -145,6 +145,7 @@ export function createSendWithGasConstraints1559<A extends TxMeta, CC extends Co
     return combineLatest(estimateGas(context, callData, args), gasPrice$).pipe(
       first(),
       switchMap(([gas, gasPrice]) => {
+        console.log("Inside sending transaction createSendWithGasConstraints1559");
         return createSendTransaction(send, context)(
           {
             ...callData,
